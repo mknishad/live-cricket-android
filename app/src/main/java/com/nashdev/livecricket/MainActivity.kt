2f0fa6a3d06b932467f4f6a45fbf8bbb5c7d8f0d
mknishad <mknishad1@ymail.com>
2021-10-23 01:00:19 +0600

    load url

--- app/src/main/java/com/nashdev/livecricket/MainActivity.kt
+++ app/src/main/java/com/nashdev/livecricket/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nashdev.livecricket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.nashdev.livecricket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.webView.webViewClient = WebViewClient()
+    binding.webView.loadUrl("https://www.smartcric.com/")
   }
 }
